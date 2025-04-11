32a243d54f857c9106bd2c1552566f76c150d8de
everysoon <minsoon1269@naver.com>
2025-04-11 10:39:16 +0900

    feat : add payment fake client

---
+++ src/main/java/kr/hhplus/be/server/application/dataplatform/PaymentClient.java
@@ -0,0 +1,8 @@
+package kr.hhplus.be.server.application.dataplatform;
+
+import kr.hhplus.be.server.interfaces.dto.PaymentDTO;
+
+public interface PaymentClient {
+	String send(PaymentDTO.PaymentRequest request);
+	String getToken(PaymentDTO.TokenRequest request);
+}

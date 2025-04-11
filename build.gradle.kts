ca336dbfda01289d91ceaa1ddbb69169b13fdfef
everysoon <minsoon1269@naver.com>
2025-04-11 10:31:41 +0900

    init : project default init

---
+++ build.gradle.kts
@@ -0,0 +1,71 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.4.1"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+fun getGitHash(): String {
+    return providers.exec {
+        commandLine("git", "rev-parse", "--short", "HEAD")
+    }.standardOutput.asText.get().trim()
+}
+
+group = "kr.hhplus.be"
+version = getGitHash()
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+tasks.test {
+    useJUnitPlatform()
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2024.0.0")
+    }
+}
+
+dependencies {
+    // Spring
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // DB
+    runtimeOnly("com.mysql:mysql-connector-j")
+
+    // Test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:mysql")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    // RestAssured (API 테스트용)
+    testImplementation("io.rest-assured:rest-assured:5.4.0")
+
+
+    // Spring boot 3.x 이상 QueryDsl
+//	implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+//	annotationProcessor("com.querydsl:querydsl-apt:5.0.0:jakarta")
+//	annotationProcessor("jakarta.annotation:jakarta.annotation-api")
+//	annotationProcessor("jakarta.persistence:jakarta.persistence-api")
+    // swagger
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+
+    // lombok
+    compileOnly("org.projectlombok:lombok:1.18.34")
+    annotationProcessor("org.projectlombok:lombok:1.18.34")
+    testCompileOnly("org.projectlombok:lombok:1.18.34")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.34")
+
+
+}
+

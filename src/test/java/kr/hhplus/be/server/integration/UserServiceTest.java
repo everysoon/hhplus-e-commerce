4c9a9ca75e3987dd6099b445a02b40b6f3f91b69
everysoon <minsoon1269@naver.com>
2025-04-18 02:03:02 +0900

    test : userService 통합 테스트 완료

---
+++ src/test/java/kr/hhplus/be/server/integration/UserServiceTest.java
@@ -0,0 +1,41 @@
+package kr.hhplus.be.server.integration;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import kr.hhplus.be.server.application.user.UserService;
+import kr.hhplus.be.server.domain.user.User;
+import kr.hhplus.be.server.domain.user.repository.UserRepository;
+import kr.hhplus.be.server.integration.common.BaseIntegrationTest;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserServiceTest extends BaseIntegrationTest {
+	@Autowired
+	private UserRepository userRepository;
+	private UserService userService;
+	@BeforeAll
+	void setup() {
+		userService = new UserService(userRepository);
+	}
+	@Test
+	void 유저_정보_조회_정상(){
+		/**
+		 * userId :1
+		 * address : 713 Mickey Drive, Diamouth, FL 18534
+		 * email : vashti.nienow@hotmail.com
+		 * name : Rusty Schroeder IV
+		 */
+		//given
+		Long userId = 1L;
+		//when
+		User user = userService.get(userId);
+		//then
+		assertAll("유저 상세 조회 정보 체크",
+			() -> assertThat(user.getEmail()).isEqualTo("vashti.nienow@hotmail.com"),
+			() -> assertThat(user.getName()).isEqualTo("Rusty Schroeder IV"),
+			() -> assertThat(user.getAddress()).isEqualTo("713 Mickey Drive, Diamouth, FL 18534")
+		);
+	}
+}

8271c880398555833c4c26c3073812f2035de579
everysoon <minsoon1269@naver.com>
2025-04-23 13:36:07 +0900

    refactor : webConfig로 filterConfig 병합

--- src/main/java/kr/hhplus/be/server/support/config/WebConfig.java
+++ src/main/java/kr/hhplus/be/server/support/config/WebConfig.java
@@ -1,7 +1,10 @@
 package kr.hhplus.be.server.support.config;
 
+import kr.hhplus.be.server.infra.web.filter.LoggingFilter;
 import kr.hhplus.be.server.infra.web.interceptor.RateLimitInterceptor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -14,5 +17,13 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(rateLimitInterceptor).addPathPatterns("/**");
+	}
+
+	@Bean
+	public FilterRegistrationBean<LoggingFilter> loggingFilter() {
+		FilterRegistrationBean<LoggingFilter> registration = new FilterRegistrationBean<>();
+		registration.setFilter(new LoggingFilter());
+		registration.addUrlPatterns("/**");
+		return registration;
 	}
 }

34fca87df2d349e43f42686709b79a3cc8f6fdf5
everysoon <minsoon1269@naver.com>
2025-05-28 17:27:40 +0900

    feat : consumer 멱등성 처리를 위한 이벤트 별 uniqueKey 검사 AOP 구현

---
+++ src/main/java/kr/hhplus/be/server/domain/order/event/OrderPlacedEvent.java
@@ -0,0 +1,37 @@
+package kr.hhplus.be.server.domain.order.event;
+
+import kr.hhplus.be.server.application.payment.PaymentCommand;
+import kr.hhplus.be.server.domain.order.Order;
+import kr.hhplus.be.server.domain.payment.PaymentMethod;
+
+import java.math.BigDecimal;
+
+public record OrderPlacedEvent(
+	Long userId,
+	Long orderId,
+	BigDecimal totalPrice,
+	PaymentMethod paymentMethod,
+	String requestId
+) {
+	public static OrderPlacedEvent of(Long userId, Order order, PaymentMethod paymentMethod,String requestId) {
+		return new OrderPlacedEvent(
+			userId,
+			order.getId(),
+			order.getTotalPrice(),
+			paymentMethod,
+			requestId
+		);
+	}
+	public PaymentCommand.Request toPayRequest(){
+		return new PaymentCommand.Request(
+				"MID_SOON_STORE",
+				totalPrice,
+				"SECRET_MERCHANT_KEY",
+				orderId,
+				paymentMethod
+		);
+	}
+	public String getIdempotencyKey() {
+		return "order:placed".concat(requestId);
+	}
+}
